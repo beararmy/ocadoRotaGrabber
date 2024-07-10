938958e5c77ff820ee59f3f3834d0008eddd2a7d
Stef Harrington-Palmer <stef.hp@cloudreach.com>
2024-07-10 21:34:41 +0100

    feat: idea of names

---
+++ functions.ps1
@@ -0,0 +1,8 @@
+function Test-RotaConnections {}
+function Get-RotaScreenshotsForProcessing {}
+function Submit-RotaScreenshotForProcessing {}
+function Get-RotaProcessedScreenshot {}
+function Get-RotaListOfShifts {}
+function Get-RotaCurrentGoogleCalendar {}
+function Add-RotaGoogleCalendarEntry {}
+function Invoke-RotaCleanup {}
