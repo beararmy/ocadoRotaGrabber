36c044c726149f969d979cd5d90e682eabde131f
Stef Harrington-Palmer <stef.hp@cloudreach.com>
2024-07-10 22:06:05 +0100

    feat: Get-RotaScreenshotsForProcessing

--- functions.ps1
+++ functions.ps1
@@ -1,5 +1,37 @@
 function Test-RotaConnections {}
-function Get-RotaScreenshotsForProcessing {}
+function Get-RotaScreenshotsForProcessing {
+    param (
+        $storage_accountKey = ( Get-AzAutomationVariable -Name storage_accountKey ),
+        $storage_accountName = ( Get-AzAutomationVariable -Name storage_accountName ),
+        $storage_fileshareName = ( Get-AzAutomationVariable -Name storage_fileshareName ),
+        $storage_blobContainerName = ( Get-AzAutomationVariable -Name storage_blobContainerName ),
+        $storage_accountContext = ( New-AzStorageContext -StorageAccountName $storage_accountName -StorageAccountKey $storage_accountKey ),
+        [bool]$listOnly = $false
+    )
+    try {
+        # Move everything from File share into blob, Document Intelligence needs blobs
+        $currentFiles = Get-AzStorageFile -ShareName $storage_fileshareName -context $storage_accountContext
+        foreach ($file in $currentFiles.Name) {
+            Start-AzStorageBlobCopy -Context $storage_accountContext -SrcShareName $storage_fileshareName -DestContext $storage_accountContext -DestContainer $storage_blobContainerName -DestBlob $file -SrcFilePath $file -Force | Out-Null
+        }
+
+        $blobs = Get-AzStorageBlob -Container $storage_blobContainerName -Context $storage_accountContext
+
+        if ($blobs.Count -gt 0) {
+            $return = $blobs.BlobBaseClient.Uri.AbsoluteUri
+        }
+        else {
+            Write-Error "No blobs returned"
+        }
+    }
+    catch {
+        $return = @{
+            status = "failed"
+            value  = "Unable to get any blobs to process"
+        }
+    }
+    return $return
+}
 function Submit-RotaScreenshotForProcessing {}
 function Get-RotaProcessedScreenshot {}
 function Get-RotaListOfShifts {}
